945860fab2549643a3bc5dd4c14ecf0aea3384da
Karthicrajamo <haikarthicraja4@gmail.com>
2026-06-11 09:29:58 +0530

    Initial commit

---
+++ src/main/java/com/assessement/upi_payment/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.assessement.upi_payment.controller;
+
+public class UserController {
+}
---
+++ src/test/java/com/assessement/upi_payment/UpiPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessement.upi_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
